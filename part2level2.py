ef0e9e1cc5b6c3eb1b28b4257e163402f6318145
vaish-navi09 <vaishanvisingh6511@gmail.com>
2026-09-01 20:51:18 -0700

    sliding + zero count , freq map ,fixed window transformation

--- part2level2.py
+++ part2level2.py
@@ -27,7 +27,7 @@ while right < len(nums):
      if length > longest:
           longest = length
      right += 1
-print(longest)
+print(longest, "longest subarray atmost k")
 
 # longest substring without repeating chaar
 s = "abcabcbb"
@@ -42,7 +42,7 @@ while right< len(s):
      seen.add(s[right])
      length = max(length, len(seen))
      right += 1
-print(length)
+print(length, "with repe longest substring")
 # mini size subarray sum 
 nums = [2,3,1,2,4,3]
 target = 7
@@ -59,7 +59,70 @@ while right < len(nums):
                total -=nums[left]
                left += 1
      right += 1
-print(smallest)
+print(smallest,"mini size subarray")
+
+
+#longest sequence consecutive with one deletion
+
+
+nums = [1,1,0,1,1,1,0,1]
+right = 0
+left = 0
+maximum = 0
+zeros= 0
+while right < len(nums):
+     if nums[right] == 0:
+          zeros += 1
+          while zeros > 1:
+               if nums[left] == 0:
+                    zeros -= 1
+               left += 1
+     length = right - left + 1
+     if length > maximum :
+          maximum = length
+     right += 1
+print(maximum - 1,"with one del longest consecutive")
+
+
+#at most 2 distint values 
+nums = [1,2,1,2,3,2,2]   
+freq = {}         
+right = 0
+left = 0
+longest = 0
+while right < len(nums):
+     freq[nums[right]]  = freq.get(nums[right],0)+ 1
+     while len(freq)> 2:
+          freq[nums[left]] -= 1
+          if freq[nums[left]] == 0:
+               del freq[nums[left]]
+          left += 1
+     length = right-left +1
+     if length > longest:
+           longest = length
+     right += 1
+print(longest,"at most two distint value")
+
+# maximum points from cards only k card
+cards = [1,2,3,4,5,6,1]
+n = len(cards)
+k = 3
+window = n-k
+total = sum(cards)
+curr = sum(cards[:window])
+minimum = curr
+right = window
+left = 0
+while right < n:
+     curr += cards[right]
+     curr -= cards[left]
+     if curr < minimum:
+          minimum = curr 
+     left += 1
+     right += 1
+print(total - minimum,"max sum of k cards")
+
+
      
 
      
